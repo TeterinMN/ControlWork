e069b1f5f2efa39d46d62d7ec802b744cc841fcb
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-18 18:25:28 +0900

    wrote the application code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите исходный массив строк, разделяя элементы запятыми:");
+string[] inputArray = Console.ReadLine()!.Split(',');
+
+int count = 0;
+string[] newArray = new string[inputArray.Length];
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray
+[i].Length <= 3)
+    {
+        newArray[count] = inputArray
+    [i];
+        count++;
+    }
+}
+
+Array.Resize(ref newArray, count);
+
+Console.WriteLine("Результат:");
+foreach (string s in newArray)
+{
+    Console.WriteLine(s);
+}
+
+Console.ReadLine();
